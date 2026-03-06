19ffb0fc0c43a08ca8cb0c5c8bb10931aa1c2a74
unknown <0198019@naver.com>
2026-03-06 16:43:40 +0900

    쓰레기 파일 정리

--- stories/preview/types.ts
+++
@@ -1,3 +0,0 @@
-export type FriendItem = { id: number; name: string; message: string };
-export type ChatItem = { id: number; room: string; message: string; time: string; unread: number };
-export type OpenFeedItem = { id: number; title: string; likes: number; comments: number; summary: string };
